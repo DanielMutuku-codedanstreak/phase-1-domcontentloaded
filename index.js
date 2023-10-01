0f8cfd37cc12de38344690cf070e64d634e53b9b
Daniel Mutuku <danielmakau507@gmail.com>
2023-10-01 20:28:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+//change the text on the page when DOM content has loaded
+const text = document.querySelector('#text')
+function alterText(){
+    text.textContent = 'This is really cool!'
+}
+
+document.addEventListener('DOMContentLoaded', alterText)
